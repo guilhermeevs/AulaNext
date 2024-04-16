ca9ff8afc66346e67489549f307f1e176d7b1996
Guilherme Souza <guilhermeevsouza@gmail.com>
2024-04-16 18:59:33 -0300

    Foram adicionados os assets/banners às páginas

--- pages/index.js
+++ pages/index.js
@@ -5,16 +5,21 @@ import Head from "next/head";
 export default function Home() {
   return (
     <Layout>
-      <>
-        <Head>
-          <title>Página Principal</title>
-          <meta name="keywords" content="Roupas, Calçados, Boné" />
-          <meta
-            name="description"
-            content="Encontre a melhor roupa para você"
-          />
-        </Head>
-      </>
+      <Head>
+        <title>Página Principal</title>
+        <meta name="keywords" content="Roupas, Calçados, Boné" />
+        <meta name="description" content="Encontre a melhor roupa para você" />
+      </Head>
+      <div>
+        <h1 className={styles.title}>Seja bem-vindo à nossa loja</h1>
+      </div>
+      <img
+        src="/images/camisas.jpg"
+        width="700px"
+        height="500px"
+        alt="Camisas de time"
+        className={styles["centered-image"]}
+      />
     </Layout>
   );
 }
--- pages/products/index.js
+++ pages/products/index.js
@@ -1,18 +1,26 @@
 import Link from "next/link";
 import Layout from "@/components/Layout";
+import Head from "next/head";
+import styles from "../../styles/Layout.module.css";
 
 export default function Products() {
   return (
     <Layout>
+      <Head>
+        <title>Página de Produtos</title>
+      </Head>
+      <h1 className={styles.title}>
+        Nossos produtos tem ganhado cada dia mais visibilidade
+      </h1>
       <ul>
-        <li>
-          <Link href="/">Voltar</Link>
-        </li>
         <li>
           <Link href="/products/pants">Calças</Link>
         </li>
         <li>
           <Link href="/products/shirts">Camisas</Link>
+        </li>
+        <li>
+          <Link href="/">Voltar</Link>
         </li>
       </ul>
     </Layout>
